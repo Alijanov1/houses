6f441cbbfb10dc5366bb37a14fb6622eb56ff71c
Muhammadyusuf <mukhammadyusuf.alijanov25@gmail.com>
2021-11-14 10:33:22 +0500

    I've optimized my code))

--- homework.py
+++ homework.py
@@ -2,12 +2,9 @@
 # don't use loops, use only finctions 
 import math
 def whoLivesHigher(floors,sections,apf, name1, a1,name2,a2):
-  res = a1 % (apf * floors)
-  res2 = a2 % (apf * floors)
-  result = res // apf
-  result2 = res2 // apf
-  if math.ceil(result) > math.ceil(result2):
-    return(name1)
+  result = (a1 % (apf * floors)) // apf
+  result2 = (a2 % (apf * floors)) // apf
+  if math.ceil(result) > math.ceil(result2): return(name1)
   else: return(name2)
 
 print(whoLivesHigher(10,3,3,'Elbek',69,'MS',59))
