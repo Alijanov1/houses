659d17d707ce97db0a1f32860d4098dac380079e
Muhammadyusuf_Alijanov <mukhammadyusuf.alijanov25@gmail.com>
2021-11-14 10:11:39 +0500

    Secpnf commit, I've changed some features such as ceiling the result

--- homework.py
+++ homework.py
@@ -1,15 +1,16 @@
 # find who lives higher in multi storey appartment
 # don't use loops, use only finctions 
+import math
 def whoLivesHigher(floors,sections,apf, name1, a1,name2,a2):
   res = a1 % (apf * floors)
   res2 = a2 % (apf * floors)
-  result = res / apf
-  result2 = res2 / apf
-  if result > result2:
+  result = res // apf
+  result2 = res2 // apf
+  if math.ceil(result) > math.ceil(result2):
     return(name1)
   else: return(name2)
 
-print(whoLivesHigher(10,3,3,'Elbek',45,'MS',12))
+print(whoLivesHigher(10,3,3,'Elbek',69,'MS',59))
 print(whoLivesHigher(10,3,3,'Asliddin',30,'Aziz',29))
 print(whoLivesHigher(10,3,3,'Nurali',56,'Nursultan',55))
 # it's working
